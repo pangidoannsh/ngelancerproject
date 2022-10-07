018a6316e468135a995c99ededf6b2d090a4d65f
pangidoannsh <74215225+pangidoannsh@users.noreply.github.com>
2022-10-07 17:59:59 +0800

    Update pencarianClicker.js

--- frontend/scripts/pencarianClicker.js
+++ frontend/scripts/pencarianClicker.js
@@ -2,6 +2,6 @@ const elemen = document.querySelectorAll(".item-pencarian-container");
 
 for (let i = 0; i < elemen.length; i++) {
     elemen[i].onclick = () => {
-        this.location.href = "ngelancerproject/profile.html";
+        this.location.href = "https://pangidoannsh.github.io/ngelancerproject/profile";
     }
 }
